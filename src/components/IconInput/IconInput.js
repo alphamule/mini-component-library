2c9de31d008d927977a2e71603b70901fe4542e4
Eric Wagner <github@alphamule.com>
2021-03-28 16:54:41 -0700

    completed workshops

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,14 +6,82 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const styles = {
+  small: {
+    height: 20,
+    fontSize: 14,
+    iconSize: 16,
+    borderThickness: 1,
+  },
+  large: {
+    height: 30,
+    fontSize: 18,
+    iconSize: 24,
+    borderThickness: 3,
+  }
+}
+
 const IconInput = ({
   label,
   icon,
   width = 250,
   size,
-  placeholder,
+  ...delegated
 }) => {
-  return 'TODO';
+  const style = styles[size];
+
+  return (
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper style={{ "--size": style.iconSize }}>
+        <Icon id={icon} size={style.iconSize + "px"} />
+      </IconWrapper>
+      <TextInput
+        {...delegated}
+        style={{
+          "--width": width + "px",
+          "--height": style.height / 16 + "rem",
+          "--border-thickness": style.borderThickness + "px",
+          "--font-size": style.fontSize + "px",
+        }}
+      />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.label`
+  display: block;
+  position: relative;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const TextInput = styled.input`
+  width: var(--width);
+  height: var(--height);
+  font-size: var(--font-size);
+  border: none;
+  border-bottom: var(--border-thickness) solid ${COLORS.black};
+  padding-left: var(--height);
+  color: inherit;
+  font-weight: 700;
+  outline-offset: 2px;
+
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  height: var(--size);
+`;
 
 export default IconInput;
--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,71 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const styles = {
+  small: {
+    height: 8,
+    padding: 0,
+    radius: 4,
+  },
+  medium: {
+    height: 12,
+    padding: 0,
+    radius: 4,
+  },
+  large: {
+    height: 16,
+    padding: 4,
+    radius: 8,
+  },
 };
+const ProgressBar = ({ value, size }) => {
+  const style = styles[size];
+
+  return (
+    <Wrapper
+      style={{
+        "--padding": style.padding + "px",
+        "--radius": style.radius + "px",
+      }}
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+    >
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <Mask>
+        <Bar
+          style={{
+            "--width": value + "%",
+            "--height": style.height + "px",
+          }}
+        />
+      </Mask>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  border-radius: var(--radius);
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  padding: var(--padding);
+`;
+
+const Bar = styled.div`
+  background-color: ${COLORS.primary};
+  height: 16px;
+  width: var(--width);
+  height: var(--height);
+`;
+
+const Mask = styled.div`
+  overflow: hidden;
+  border-radius: 4px;
+`;
 
 export default ProgressBar;
